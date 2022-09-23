6a3c91d4b914b09fb3f55152e4dd130ba50c2de0
Aarya Chopkar <aaryachopkar@gmail.com>
2022-09-23 22:13:04 +0530

    Completed Sidebar

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,7 +1,35 @@
-const SideBar = () => {
-    return(
-        <>SideBar</>
-    )
-};
+import { Stack } from "@mui/material";
+import {categories} from "../utils/constants"
+
+const selectedCategory = 'New';
+
+const SideBar = () => (
+    <Stack
+    direction="row"
+    sx={{
+        overflowY: "auto",
+        height: { sx: "auto", md: "94%" },
+        flexDirection: { md: "column" },
+      }} 
+    >
+        {categories.map((category) => (
+            <button
+                className="category-btn"
+                style={{
+                    background: category.name === selectedCategory && '#FC1503',
+                    color:"white"
+                }}
+                key={category.name}
+            >
+                <span style={{color: 
+                    category.name === selectedCategory ? 'white' : 'red', marginRight:"12px"}}
+                >
+                    {category.icon}
+                </span>
+                <span>{category.name}</span>
+            </button>
+        ))}
+    </Stack>
+);
 
 export default SideBar
