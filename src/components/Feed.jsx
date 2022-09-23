cadec59c097f31139c96f26f771e78db8370f10f
Aarya Chopkar <aaryachopkar@gmail.com>
2022-09-23 22:12:41 +0530

    Completed Styling

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,15 +1,17 @@
 import { useState, useEffect } from "react";
 import { Box, Stack, Typography } from "@mui/material";
+import SideBar from "./SideBar";
 
 const Feed = () => {
     return(
         <Stack sx={{ flexDirection:{ sx:"column", md:"row"}}}>
             <Box 
-            sx={{height:{sx:"auto", md:"92vh"}, borderRight:"1px solid grey", px:{sx:0, md:2}}}
+            sx={{ height: { sx: "auto", md: "92vh" }, borderRight: "1px solid #3d3d3d", px: { sx: 0, md: 2 }}}
             >
-                SideBar
+                <SideBar/>
                 <Typography 
-                className="copyright" variant="body2" sx={{mt:1.5, color:"#fff"}}
+                className="copyright" variant="body2" 
+                sx={{ color:"#fff"}}
                 >
                     Copyright &copy; <a href="https://accodes21.github.io/Profile/" target="blank" 
                     style={{color:"#fff"}}
