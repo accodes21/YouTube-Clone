0287553c1950f931bacd5ab31b254f4a7537e948
Aarya Chopkar <aaryachopkar@gmail.com>
2022-09-25 23:04:58 +0530

    On click func added

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,9 +1,7 @@
 import { Stack } from "@mui/material";
 import {categories} from "../utils/constants"
 
-const selectedCategory = 'New';
-
-const SideBar = () => (
+const SideBar = ({selectedCategory, setselectedCategory}) => (
     <Stack
     direction="row"
     sx={{
@@ -15,6 +13,7 @@ const SideBar = () => (
         {categories.map((category) => (
             <button
                 className="category-btn"
+                onClick={() => setselectedCategory(category.name)}
                 style={{
                     background: category.name === selectedCategory && '#FC1503',
                     color:"white"
