572846a532770b0cb6fce2908d98a9fd974e5328
Aarya Chopkar <aaryachopkar@gmail.com>
2022-09-25 23:04:11 +0530

    Video & api

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,14 +1,26 @@
 import { useState, useEffect } from "react";
 import { Box, Stack, Typography } from "@mui/material";
 import SideBar from "./SideBar";
+import Videos from "./Videos";
+import { api } from "../utils/api";
 
 const Feed = () => {
+
+    const [selectedCategory, setselectedCategory] = useState("New")
+
+    useEffect(() => {
+        api(`search?part=snippet&q=${selectedCategory}`)
+    }, [selectedCategory])
+    
     return(
         <Stack sx={{ flexDirection:{ sx:"column", md:"row"}}}>
             <Box 
             sx={{ height: { sx: "auto", md: "92vh" }, borderRight: "1px solid #3d3d3d", px: { sx: 0, md: 2 }}}
             >
-                <SideBar/>
+                <SideBar
+                    selectedCategory={selectedCategory}
+                    setselectedCategory={setselectedCategory}
+                />
                 <Typography 
                 className="copyright" variant="body2" 
                 sx={{ color:"#fff"}}
@@ -19,6 +31,9 @@ const Feed = () => {
                         Aarya Chopkar
                     </a>
                 </Typography>
+            </Box>
+            <Box>
+                <Videos videos={[]}/>
             </Box>
         </Stack>
     )
