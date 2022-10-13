4326bb540399e49e1dd409b7739c7d25519b149c
Aarya Chopkar <aaryachopkar@gmail.com>
2022-10-13 23:09:18 +0530

    Added onClick

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -32,8 +32,12 @@ const Feed = () => {
                     </a>
                 </Typography>
             </Box>
-            <Box>
-                <Videos videos={[]}/>
+            <Box p={2} sx={{ overflowY: "auto", height: "90vh", flex: 2 }}>
+                <Typography variant="h4" fontWeight="bold" mb={2} sx={{ color: "white" }}>
+                    {selectedCategory} <span style={{ color: "#FC1503" }}>videos</span>
+                </Typography>
+
+                {/* <Videos videos={videos} /> */}
             </Box>
         </Stack>
     )
