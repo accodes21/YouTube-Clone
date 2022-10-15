41f32394ad69b68db4e8bbff0e4b1a047980174d
Aarya Chopkar <aaryachopkar@gmail.com>
2022-10-15 22:06:35 +0530

    Video data fetched

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -1,6 +1,18 @@
-const Videos = () => {
+import { Stack, Box } from "@mui/material";
+import ChannelCard from "./ChannelCard";
+import VideoCard from "./VideoCard";
+
+const Videos = ({videos}) => {
+    // console.log(videos)
     return(
-        <>Videos</>
+        <Stack direction="row" flexWrap="wrap" justifyContent="start" gap={2}>
+            {videos.map((item, id) => (
+                <Box key={id}>
+                    {item.id.videoId && <VideoCard video={item}/>}
+                    {item.id.channelId && <ChannelCard channelDetail={item}/>}
+                </Box>
+            ))}
+        </Stack>
     )
 };
 
